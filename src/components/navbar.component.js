8a7780f2d8a62dcac10428f1c201db1e1105c174
Gina Frankel <g.frankel22@gmail.com>
2020-07-29 17:05:43 +0100

    Add Links for index, create + user pages

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,11 +1,14 @@
-import React, {component} from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export default class NavBar extends Component  {
-
-  render(){
-    return(
-   
-    )
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <Link to="/"> Exercise Tracker</Link>
+        <Link to="/create">Create Exercise</Link>
+        <Link to="/user"> Create User</Link>
+      </nav>
+    );
   }
 }
