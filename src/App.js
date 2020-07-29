844d7dc38b29920c084756926c20ec69cc9c18a7
Gina Frankel <g.frankel22@gmail.com>
2020-07-29 17:04:11 +0100

    Fix bootstrap install + comment out unused routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
-import "boostrap/dist/css/boostrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import CreateExercise from "./components/create-exercise.component"
-import CreateUser from "./components/create-user.component"
-import EditExercise from "./components/edit-exercise.component"
-import ExercisesList from "./components/exercises-list.component"
-import NavBar from "./components/navbar.component"
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import EditExercise from "./components/edit-exercise.component";
+import ExercisesList from "./components/exercises-list.component";
+import NavBar from "./components/navbar.component";
 
 function App() {
- 
-  
-<Router>
-  <NavBar />
-  {/* loads component depending on path */}
-  <Route path="/" exact component={ExercisesList}/>
-  <Route path ="/edit/:id" component={CreateExercise}/>
-  <Route path="/create" component={CreateExercise}/>
-  <Route path="/user" component={CreateUser}/>
-
-</Router>
-
-  
-  
+  return (
+    <Router>
+      <NavBar />
+      {/* loads component depending on path */}
+      {/* <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} /> */}
+    </Router>
+  );
+}
 
 export default App;
