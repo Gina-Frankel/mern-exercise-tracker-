57bbd6ee1e84483f13f8097d04ef479cb2a53094
Gina Frankel <g.frankel22@gmail.com>
2020-07-29 18:47:37 +0100

    Style with bootstrap Navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -4,10 +4,20 @@ import { Link } from "react-router-dom";
 export default class NavBar extends Component {
   render() {
     return (
-      <nav>
-        <Link to="/"> Exercise Tracker</Link>
-        <Link to="/create">Create Exercise</Link>
-        <Link to="/user"> Create User</Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          {" "}
+          Exercise Tracker
+        </Link>
+
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link to="/create">Create Exercise</Link>
+          </li>
+          <li className="nav-item active">
+            <Link to="/user"> Create User</Link>
+          </li>
+        </ul>
       </nav>
     );
   }
