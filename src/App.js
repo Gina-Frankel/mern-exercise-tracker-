39294400e745252f561daee4fd990c0c690d2ff9
Gina Frankel <g.frankel22@gmail.com>
2020-07-29 16:51:22 +0100

    Delete boilerplate import components + boostrap + reactRouter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "boostrap/dist/css/boostrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateExercise from "./components/create-exercise.component"
+import CreateUser from "./components/create-user.component"
+import EditExercise from "./components/edit-exercise.component"
+import ExercisesList from "./components/exercises-list.component"
+import NavBar from "./components/navbar.component"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
+  
+<Router>
+  <NavBar />
+  {/* loads component depending on path */}
+  <Route path="/" exact component={ExercisesList}/>
+  <Route path ="/edit/:id" component={CreateExercise}/>
+  <Route path="/create" component={CreateExercise}/>
+  <Route path="/user" component={CreateUser}/>
+
+</Router>
+
+  
+  
 
 export default App;
